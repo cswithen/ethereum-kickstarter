7a4c8e969c6a15fa45ba7434ab4f6d3ca49a3cff
cswithen <cody.swithenbank@gmail.com>
2021-11-24 11:10:08 -0800

    fix: css rendering
    fixed css rendering as NextJS does not support modular css, included basic link tag to the react component to show styling
    
    closes #54

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,15 @@ class CampaignIndex extends Component {
   }
 
   render() {
-    return <div>{this.renderCampaigns()}</div>;
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        ></link>
+        {this.renderCampaigns()}
+      </div>
+    );
   }
 }
 
