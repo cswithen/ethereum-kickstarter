97573ed1f920da6e9b5218f4b45e3109fc82794b
cswithen <cody.swithenbank@gmail.com>
2021-11-29 11:09:01 -0800

    feat: request row component
    created a request row component that will render logic to display on the view requests page. Used semantic-ui's Table.Row and Table.Cell components to render information for each specific table header. Added in a additional call to the requests page to pass down total approversCount from the campaig
    
    closes #129

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,12 +3,14 @@ import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query;
     const campaign = Campaign(address);
     const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
     const requests = await Promise.all(
       Array(parseInt(requestCount))
@@ -18,7 +20,21 @@ class RequestIndex extends Component {
         })
     );
 
-    return { address, requests, requestCount };
+    return { address, requests, requestCount, approversCount };
+  }
+
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          request={request}
+          id={index}
+          key={index}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
   }
 
   render() {
@@ -45,6 +61,7 @@ class RequestIndex extends Component {
               <HeaderCell>Finalize</HeaderCell>
             </Row>
           </Header>
+          <Body>{this.renderRows()}</Body>
         </Table>
       </Layout>
     );
