9e81e7fc2a442360ba6f1d27a2d92f379a12eaec
cswithen <cody.swithenbank@gmail.com>
2021-12-18 23:10:53 -0800

    fix: deployed address
    added address that contract was deployed to, to the factory.js file

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 // import { deployedAddress } from "../secrets";
 
-const deployedAddress = process.env.DEPLOYED_ADDRESS;
+const deployedAddress = "0x1C8179d41A690aD991B8C0A23357519C57742D59";
 
 const instance = new web3.eth.Contract(CampaignFactory.abi, deployedAddress);
 
